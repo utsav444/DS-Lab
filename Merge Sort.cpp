9b3a36c298ad3af0476aafbc294357766fbe8728
Utsav Sangani <124497683+utsav444@users.noreply.github.com>
2023-11-02 00:06:10 +0530

    Update Merge Sort.cpp

--- Merge Sort.cpp
+++ Merge Sort.cpp
@@ -1,52 +1,49 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void merge_sort(int a[], int lb, int ub)
-{
-    if (lb < ub)
-    {
+void merge(int arr[], int lb, int ub) {
+    if (lb < ub) {
         int mid = (lb + ub) / 2;
-        int b[ub - lb + 1];
+        int *b = (int *)malloc((ub - lb + 1) * sizeof(int));
         int i = lb, j = mid + 1, m = 0, k;
 
-        while (i <= mid && j <= ub)
-        {
-            if (a[i] < a[j])
-            {
-                b[m] = a[i];
+        while (i <= mid && j <= ub) {
+            if (arr[i] < arr[j]) {
+                b[m] = arr[i];
                 i++;
-            }
-            else
-            {
-                b[m] = a[j];
+            } else {
+                b[m] = arr[j];
                 j++;
             }
             m++;
         }
-
-        while (i <= mid)
-        {
-            b[m] = a[i];
+        while (i <= mid) {
+            b[m] = arr[i];
             i++;
             m++;
         }
-
-        while (j <= ub)
-        {
-            b[m] = a[j];
+        while (j <= ub) {
+            b[m] = arr[j];
             j++;
             m++;
         }
-
-        for (k = 0; k < m; k++)
-        {
-            a[lb + k] = b[k];
+        for (k = 0; k < m; k++) {
+            arr[lb + k] = b[k];
         }
+        free(b);
     }
 }
 
+void merge_sort(int arr[], int lb, int ub) {
+    if (lb < ub) {
+        int mid = (lb + ub) / 2;
+        merge_sort(arr, lb, mid);      // Sort the left subarray
+        merge_sort(arr, mid + 1, ub);  // Sort the right subarray
+        merge(arr, lb, ub);            // Merge the two sorted subarrays
+    }
+}
 
-int main()
-{
+int main() {
     int arr[100];
     int arr_size;
 
@@ -54,19 +51,16 @@ int main()
     scanf("%d", &arr_size);
 
     printf("Enter the elements of the array: ");
-    for (int i = 0; i < arr_size; i++)
-    {
+    for (int i = 0; i < arr_size; i++) {
         scanf("%d", &arr[i]);
     }
 
     merge_sort(arr, 0, arr_size - 1);
 
     printf("Sorted array: ");
-    for (int i = 0; i < arr_size; i++)
-    {
+    for (int i = 0; i < arr_size; i++) {
         printf("%d ", arr[i]);
     }
-    
+
     return 0;
 }
-
